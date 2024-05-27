adafacbc936105c93f23317b9b3fdcf4e59d7a09
naaltunian <naaltunian@icloud.com>
2024-05-27 15:12:24 -0400

    Solution

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 // fix the error 
-const cat1 ='Whiskers'
+let cat1 ='Whiskers'
 cat1 = 'Delilah'
 
 // select the first cat and change the name to the above variable
-const cat1TextContent = document.getElementById('').textContent = cat1
+const cat1TextContent = document.getElementById('cat-1-name').textContent = cat1
 
 // the first adopt button should remove the first cat when clicked
-document.getElementById('').addEventListener('', () => {
-  document.getElementById('cat-1').delete()
+document.getElementById('cat-1').addEventListener('click', () => {
+  document.getElementById('cat-1').remove()
 })
 
 // the second adopt button should remove the second cat when clicked
-document.querySelector('cat-2').addEventListener('', () => {
-  document.getElemeentById('cat-2').delete()
+document.querySelector('#cat-2').addEventListener('click', () => {
+  document.getElementById('cat-2').remove()
 })
 
 // write code to remove the third cat when clicked
+document.getElementById('cat-3').addEventListener('click', () => {
+  document.getElementById('cat-3').remove()
+})
